9d2c32cc85489d42cbec680a040c907bbbbfcc4c
seob <kseob7@naver.com>
2023-02-22 18:05:43 +0900

    [✨ feat] 중복되는 함수 재활용

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-  const [author, setAuthor] = useState("");
-  const [content, setContent] = useState("");
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
-          value={author}
-          onChange={(e) => {
-            setAuthor(e.target.value);
-          }}
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
-          value={content}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
         />
       </div>
     </div>
