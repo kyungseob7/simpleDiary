9675cada820677552d33d48d162d7ca34fd46dd7
seob <kseob7@naver.com>
2023-02-23 09:42:25 +0900

    [✨feat] 적정 글자 수 미만일 경우 텍스트에 focus 주는 기능

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -16,16 +19,15 @@ const DiaryEditor = () => {
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
-      alert("작성자는 최소 1글자 이상 입력해주세요");
+      authorInput.current.focus();
       return;
     }
 
     if (state.content.length < 5) {
-      alert("일기 본문은 최소 5글자 이상 입력해주세요");
+      contentInput.current.focus();
       return;
     }
 
-    console.log(state);
     alert("저장 성공");
   };
 
@@ -35,6 +37,7 @@ const DiaryEditor = () => {
       <div>
         <input
           name="author"
+          ref={authorInput}
           value={state.author}
           onChange={handleChangeState}
         />
@@ -42,6 +45,7 @@ const DiaryEditor = () => {
       <div>
         <textarea
           name="content"
+          ref={contentInput}
           value={state.content}
           onChange={handleChangeState}
         />
