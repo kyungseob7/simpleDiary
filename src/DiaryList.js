5ddfc44ff6885d4e41e4cd8f9f67029824d5a814
seob <kseob7@naver.com>
2023-02-23 11:09:01 +0900

    [✨feat] 리스트 객체 넣기, 리스트 기본값 넣기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -3,8 +3,23 @@ const DiaryList = ({ diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <div key={it.id}>
+            <div>작성자 : {it.author}</div>
+            <div>일기 : {it.content}</div>
+            <div>감정 : {it.emotion}</div>
+            <div>작성시간 : {it.created_date}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
 };
 
 export default DiaryList;
