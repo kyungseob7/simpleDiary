d3d9979fa2d06660fbe33972541feedd929d7a7b
seob <kseob7@naver.com>
2023-02-23 10:43:52 +0900

    [✨feat] DiaryList 생성 후 App.js 연결 후 리스트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "변경섭",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "하이2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "장재국",
+    content: "하이3",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
